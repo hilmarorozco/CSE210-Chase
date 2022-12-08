33e60294107f0cce8541b350ceb054b6f63b8338
FIRST_NAME LAST_NAME <jschro678@gmail.com>
2022-12-07 18:10:53 -0700

    Added multiple coins

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Unit05.Game.Scripting;
 using Unit05.Game.Services;
 using Unit05.Game;
 
-
 namespace Unit05
 {
     /// <summary>
@@ -19,19 +18,23 @@ namespace Unit05
         static void Main(string[] args)
         {
             Cycle cycleA = new Cycle();
-            cycleA.PrepareBody(new Point(225,150));
+            cycleA.PrepareBody(new Point(225, 150));
             //cycleA.PrepareBody(new Point(-225,150), Constants.RED);
-            
+
             Cycle cycleB = new Cycle();
-            cycleB.PrepareBody(new Point(-225,150));
+            cycleB.PrepareBody(new Point(-225, 150));
             //cycleB.PrepareBody(new Point(225,150), Constants.BLUE);
-            
+
 
             // create the cast
             Cast cast = new Cast();
             cast.AddActor("cycleA", cycleA);
             cast.AddActor("cycleB", cycleB);
-            cast.AddActor("food", new Food());
+            Food food = new Food();
+            food.Reset(cast);
+            Food obstacle = new Food();
+
+            obstacle.Reset(cast);
             cast.AddActor("score", new Score());
             // cast.AddActor("scoreA", new ScoreA());
             // cast.AddActor("scoreB", new ScoreB());
@@ -40,7 +43,7 @@ namespace Unit05
             // create the services
             KeyboardService keyboardService = new KeyboardService();
             VideoService videoService = new VideoService(false);
-           
+
             // create the script
             Script script = new Script();
             script.AddAction("input", new ControlActorsAction(keyboardService));
